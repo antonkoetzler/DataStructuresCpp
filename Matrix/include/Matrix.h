e8e43725e1fcfee5492e202d16120c6fb2eafef5
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2021-04-19 16:40:29 -0600

    Matrix program added

--- Matrix/include/Matrix.h
+++ Matrix/include/Matrix.h
@@ -9,13 +9,16 @@ public:
 	Matrix() {}
 	Matrix(int); // x by x
 	Matrix(int, int); // x by y
+	~Matrix();
 
 	// Allocation
 	void allocateMatrix(int, int);
 	void replaceValue(int, int, int);
+	void readMatrix();
+	void resizeMatrix(int, int);
 
 	// Printer
-	void printMatrix();
+	void printMatrix() const;
 
 private:
 	std::vector<std::vector<int>> matrix;
--- Matrix/src/Matrix.cpp
+++ Matrix/src/Matrix.cpp
@@ -10,6 +10,11 @@ Matrix::Matrix(int x, int y) // Dimension args
 	allocateMatrix(x, y);
 }
 
+Matrix::~Matrix()
+{
+	matrix.clear();
+}
+
 void Matrix::allocateMatrix(int x, int y) // Dimension args
 {
 	std::vector<int> row;
@@ -25,10 +30,58 @@ void Matrix::allocateMatrix(int x, int y) // Dimension args
 
 void Matrix::replaceValue(int x, int y, int value)
 {
-	
+	for (int i = 0; i < (int)matrix.size(); i++)
+	{
+		for (int o = 0; o < (int)matrix[i].size(); o++)
+		{
+			if (i == x && o == y)
+				matrix[i][o] = value;
+		}
+	}
 }
 
-void Matrix::printMatrix()
+void Matrix::readMatrix()
+{
+	int value;
+
+	for (int i = 0; i < (int)matrix.size(); i++)
+	{
+		for (int o = 0; o < (int)matrix[i].size(); o++)
+		{
+			std::cout << "Enter value for (" << i << ", " << o << "): ";
+			std::cin >> value;
+
+			matrix[i][o] = value;
+		}
+	}
+}
+
+void Matrix::resizeMatrix(int x, int y) // New dimensions
+{
+	std::vector<int> row;
+	std::vector<std::vector<int>> newMatrix;
+
+	for (int i = 0; i < x; i++)
+	{
+		for (int o = 0; o < y; o++)
+		{
+			if (i > (int)matrix.size() - 1)
+				row.push_back(0);
+			else if (o > (int)matrix[i].size() - 1)
+				row.push_back(0);
+			else
+				row.push_back(matrix[i][o]);
+		}
+
+		newMatrix.push_back(row);
+		row.clear();
+	}
+
+	matrix.clear(); matrix = newMatrix;
+	row.clear(); newMatrix.clear();
+}
+
+void Matrix::printMatrix() const
 {
 	for (int i = 0; i < (int)matrix.size(); i++)
 	{
@@ -42,4 +95,6 @@ void Matrix::printMatrix()
 				std::cout << std::endl << std::endl;
 		}
 	}
+
+	std::cout << std::endl << std::endl;
 }
