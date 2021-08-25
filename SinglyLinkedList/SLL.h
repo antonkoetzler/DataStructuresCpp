36dbe5c9cbc4c1706239becb2fa66493c0036862
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2021-08-24 21:25:07 -0600

    Added my code

--- SinglyLinkedList/SLL.h
+++ SinglyLinkedList/SLL.h
@@ -1,18 +1,119 @@
-// Program: Singly Linked List
-//
-// Operations
-// - add(value) ~ Adds value to the last index of the list
-// - add(index, value) ~ Adds value to the index provided
-// - del() ~ Deletes the last index of the list
-// - del(value) ~ Deletes the value provided
-// - getData() ~ Returns the entire list
-// - getDataAsFile() ~ Returns the entire list as a text file
-// - print()
-// - search(value) ~ Searches and returns the element if found
-//
-// Features
-// - Works with any type of data
-// - One file
-// - Get your data within a text file
-//
-// Author: Anton Koetzler-Faust
+/*
+Program: Singly Linked List
+
+Operations
+- add(value) ~ Adds a new value to the end of the list
+- add(value, index) ~ Adds a new value to the index provided
+- del() ~ Deletes the end element of the list
+- del(value) ~ Deletes the value provided
+- del(index) ~ Deletes the value at the index provided
+- search(value) ~ Searches for a value and returns true if it's in the list
+- getElement(index) ~ Get's the element's data at the index provided
+- getData() ~ Get's the list and converts it into a text file
+- print() ~ Prints the list
+
+Features
+- One header file (easy to implement)
+- Can create a text file of your list
+
+Author: Anton Koetzler-Faust
+*/
+
+#pragma once
+
+#include <iostream>
+
+template <class T>
+class SLL
+{
+public:
+	SLL()
+	{
+		data = nullptr;
+		next = nullptr;
+	}
+	SLL(T newData)
+	{
+		next = nullptr;
+		data = new T(newData);
+	}
+	~SLL()
+	{
+		delete next;
+	}
+
+	void add(T newData)
+	{
+		// Checking if we have an empty list
+		if (data == nullptr)
+			data = new T(newData);
+		else if (next == nullptr)
+			next = new SLL<int>(newData);
+		else
+			next->add(newData);
+	}
+
+	void add(T newData, int index)
+	{
+		static int counter = 0; // Record where we are recursing to
+		static int size;        // Get the size of the array
+
+		// Making sure we don't have an index greater than the list
+		if (counter == 0)
+		{
+			size = this->size();
+			if (index > size)
+			{
+				std::cout << "Invalid index provided" << std::endl;
+				return;
+			}
+		}
+
+		counter++;
+
+		if (counter == index)
+			std::cout << "counter == index" << std::endl;
+		else if (next != nullptr)
+			next->add(newData, index);
+	}
+
+	void print()
+	{
+		// Checking if we have an empty list
+		if (data == nullptr)
+			std::cout << "This is an empty singly linked list." << std::endl;
+
+		std::cout << *data;
+
+		if (next == nullptr)
+			std::cout << std::endl;
+		else
+		{
+			std::cout << ", ";
+			next->print();
+		}
+	}
+
+	int size()
+	{
+		static int size = 0;
+
+		// Empty list condition
+		if (data == nullptr)
+			return size;
+
+		size++;
+
+		if (next == nullptr)
+		{
+			int returnSize = size;
+			size = 0; // Resetting the size
+			return returnSize;
+		}
+		else
+			return next->size();
+	}
+private:
+	T* data;
+	SLL<T>* next;
+};
--- SinglyLinkedList/main.cpp
+++ SinglyLinkedList/main.cpp
@@ -2,7 +2,17 @@
 
 int main()
 {
-	SLL* foo = new SLL();
-	
+	SLL<int>* foo1 = new SLL<int>();
+	SLL<int>* foo2 = new SLL<int>(5);
+
+	foo1->add(3);
+	foo1->add(2);
+	foo1->add(1);
+	foo1->add(3, 1);
+	foo1->print();
+
+	delete foo1;
+	delete foo2;
+
 	return 0;
 }
