652f74bbb3bbc6e850375d9e979ae653e3bf8c0c
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2021-08-25 15:32:17 -0600

    Updated SLL

--- SinglyLinkedList/SLL.h
+++ SinglyLinkedList/SLL.h
@@ -1,118 +1,178 @@
 /*
-Program: Singly Linked List
+Singly Linked List
 
 Operations
-- add(value) ~ Adds a new value to the end of the list
-- add(value, index) ~ Adds a new value to the index provided
-- del() ~ Deletes the end element of the list
-- del(value) ~ Deletes the value provided
-- del(index) ~ Deletes the value at the index provided
-- search(value) ~ Searches for a value and returns true if it's in the list
-- getElement(index) ~ Get's the element's data at the index provided
-- getData() ~ Get's the list and converts it into a text file
-- print() ~ Prints the list
+- add(value)
+	- Adds a value to the back of the list
+- add(value, index)
+	- Adds a value to the index provided
+- del()
+	- Deletes the back element of the list
+- del(value)
+	- Deletes the value provided
+- back()
+	- Gets the back of the list
+- front()
+	- Gets the front of the list
+- search(value)
+	- Searches for the value and returns the data if true
+- print()
+	- Prints the list
+- toFile()
+	- Converts the array to a text file
 
 Features
-- One header file (easy to implement)
-- Can create a text file of your list
+- Operation overloading, works like a normal array
+- Can turn your array into a text file
+- One file implementation
 
 Author: Anton Koetzler-Faust
 */
 
 #pragma once
-
 #include <iostream>
 
 template <class T>
 class SLL
 {
 public:
+	// Creates an empty list
 	SLL()
 	{
 		data = nullptr;
 		next = nullptr;
 	}
+
+	// Creates a 1 element list, or adds to an existing list
 	SLL(T newData)
 	{
-		next = nullptr;
 		data = new T(newData);
+		next = nullptr;
 	}
+
 	~SLL()
 	{
 		delete next;
+		delete data;
+		next = nullptr;
+		data = nullptr;
 	}
+
 
 	void add(T newData)
 	{
-		// Checking if we have an empty list
+		// Empty list
 		if (data == nullptr)
 			data = new T(newData);
-		else if (next == nullptr)
-			next = new SLL<int>(newData);
-		else
+		// Recurse
+		else if (next != nullptr)
 			next->add(newData);
+		// Add SLL to existing list
+		else
+			next = new SLL(newData);
 	}
-
-	void add(T newData, int index)
+	bool add(T newData, int index)
 	{
-		static int counter = 0; // Record where we are recursing to
-		static int size;        // Get the size of the array
+		static int counter = 0;
 
-		// Making sure we don't have an index greater than the list
+		// Error handling
 		if (counter == 0)
 		{
-			size = this->size();
-			if (index > size)
+			if (index > this->size() || index < 0)
+				return false;
+			// Makes life easier
+			else if (index == this->size())
 			{
-				std::cout << "Invalid index provided" << std::endl;
-				return;
+				this->add(newData);
+				return true;
 			}
 		}
 
-		counter++;
-
+		// Index found
 		if (counter == index)
-			std::cout << "counter == index" << std::endl;
+		{
+			counter++;
+
+			// Transfering data and making space after the add call
+			T transferData = *data;
+			delete data;
+			data = nullptr;
+			data = new T(newData);
+
+			// Makes space for the other elements
+			if (next != nullptr)
+				next->makeSpace(transferData);
+
+			return true;
+		}
+		// Recurse
 		else if (next != nullptr)
+		{
+			counter++;
 			next->add(newData, index);
-	}
-
-	void print()
-	{
-		// Checking if we have an empty list
-		if (data == nullptr)
-			std::cout << "This is an empty singly linked list." << std::endl;
-
-		std::cout << *data;
-
-		if (next == nullptr)
-			std::cout << std::endl;
+		}
+		// Object not found
 		else
 		{
-			std::cout << ", ";
-			next->print();
+			counter = 0;
+			return false;
 		}
 	}
+	void makeSpace(T transferData)
+	{
+		T transfer = *data;
+		delete data;
+		data = nullptr;
+		data = new T(transferData);
+
+		if (next != nullptr)
+			next->makeSpace(transfer);
+		else
+			next = new SLL(transfer);
+	}
+
 
 	int size()
 	{
 		static int size = 0;
 
-		// Empty list condition
+		// Empty list
 		if (data == nullptr)
-			return size;
+			return 0;
 
 		size++;
 
-		if (next == nullptr)
+		if (next != nullptr)
+			next->size();
+		else
 		{
 			int returnSize = size;
-			size = 0; // Resetting the size
+			size = 0; // Reset the static variable
 			return returnSize;
 		}
-		else
-			return next->size();
 	}
+
+
+	void print()
+	{
+		// Empty list
+		if (data == nullptr)
+		{
+			std::cout << "Empty list" << std::endl;
+			return;
+		}
+
+		std::cout << *data;
+
+		if (next != nullptr)
+		{
+			std::cout << ", ";
+			next->print();
+		}
+		else
+			std::cout << std::endl;
+	}
+
 private:
 	T* data;
 	SLL<T>* next;
--- SinglyLinkedList/main.cpp
+++ SinglyLinkedList/main.cpp
@@ -1,18 +1,17 @@
+// Demo for my Singly Linked List
+
 #include "SLL.h"
 
 int main()
 {
 	SLL<int>* foo1 = new SLL<int>();
-	SLL<int>* foo2 = new SLL<int>(5);
 
-	foo1->add(3);
-	foo1->add(2);
-	foo1->add(1);
-	foo1->add(3, 1);
+	for (int i = 0; i < 5; i++)
+		foo1->add(i);
+
+	std::cout << "foo1->add: " << foo1->add(10, 0) << std::endl;
 	foo1->print();
 
 	delete foo1;
-	delete foo2;
-
 	return 0;
 }
