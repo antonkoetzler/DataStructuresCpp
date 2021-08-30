f59b8ff245ed43334452860d5f03b62750997b62
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2021-08-30 13:43:28 -0600

    Completed stack

---
+++ Stack/Stack.h
@@ -0,0 +1,128 @@
+#pragma once
+#include <iostream>
+#include <typeinfo>
+#include <iomanip>
+
+template <class T>
+class Stack
+{
+public:
+	// Empty stack
+	Stack()
+	{
+		data = nullptr;
+		size = 0;
+	}
+	// 1 element stack
+	Stack(T obj)
+	{
+		data = new T[1];
+		data[0] = obj;
+		size = 1;
+	}
+	~Stack()
+	{
+		delete[] data;
+		data = nullptr;
+		size = 0;
+	}
+
+	void push(T obj)
+	{
+		// Empty stack case
+		if (data == nullptr)
+		{
+			data = new T[1];
+			data[0] = obj;
+			size = 1;
+		}
+		// Stack reallocation case
+		else
+		{
+			T* newData = new T[size + 1];
+
+			// Transferring our data to the new stack
+			for (int i = 0; i < size; i++)
+				newData[i] = data[i];
+
+			// Adding obj to the new stack
+			newData[size] = obj;
+
+			// Updating the size of our stack
+			size++;
+
+			delete[] data;
+			data = nullptr;
+			data = newData;
+		}
+	}
+	bool pop()
+	{
+		// Empty stack case
+		if (data == nullptr) return false;
+		// Stack reallocation case
+		else
+		{
+			// Turning the stack into an empty stack
+			if (size == 1)
+			{
+				delete[] data;
+				data = nullptr;
+				size = 0;
+			}
+			else
+			{
+				T* newData = new T[size + 1];
+
+				// Transfering data to new stack
+				for (int i = 0; i < (size - 1); i++)
+					newData[i] = data[i];
+
+				// Updating size of our stack
+				size--;
+
+				delete[] data;
+				data = nullptr;
+				data = newData;
+
+				return true;
+			}
+		}
+	}
+	bool isEmpty()
+	{
+		if (data == nullptr) return true;
+		else return false;
+	}
+	void print()
+	{
+		// Empty stack case
+		if (data == nullptr)
+		{
+			std::cout << "Empty stack" << std::endl;
+			return;
+		}
+		else
+		{
+			// Neater printing for float and double stack
+			std::string datatype;
+
+			for (int i = 0; i < size; i++)
+			{
+				datatype = typeid(data[i]).name();
+
+				if (datatype == "f" || datatype == "float" || datatype == "d" || datatype == "double")
+					std::cout << std::fixed << std::setprecision(sizeof(data[i])) << data[i];
+				else
+					std::cout << data[i];
+
+				if (i == (size - 1)) std::cout << std::endl;
+				else std::cout << ", ";
+			}
+		}
+	}
+	int getSize() { return size; }
+private:
+	T* data;  // Our array
+	int size; // Size of our array
+};
